59514a9358845a05094ff91b949532291ab15d48
Alexander Santos <alexsantosantana@live.com>
2019-08-17 14:01:30 -0400

    Working sequelize connecton errors

--- config/config.js
+++ config/config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  "development": {
-    "username": "postgres",
-    "password": "090696",
-    "database": "twitterClone",
-    "host": "127.0.0.1",
-    "dialect": "postgres",
-    "logging": false
-  },
+  // "development": {
+  //   "username": "postgres",
+  //   "password": "090696",
+  //   "database": "twitterClone",
+  //   "host": "127.0.0.1",
+  //   "dialect": "postgres",
+  //   "logging": false
+  // },
   "production": {
     "username": process.env.RDS_USERNAME,
     "password": process.env.RDS_PASSWORD,
     "database": process.env.RDS_DB_NAME,
     "host": process.env.RDS_HOSTNAME,
-    "dialect": "postgres",
+    "dialect": "postgres"
     //! For HEROKU
     // "use_env_variable": "DATABASE_URL",
     // "dialect": "postgres"
