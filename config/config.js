54efb4e8f6d37ab1e847922326a9d01d92c49e64
Alexander Santos <alexsantosantana@live.com>
2019-08-17 14:04:13 -0400

    Working with sequelize

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  // "development": {
-  //   "username": "postgres",
-  //   "password": "090696",
-  //   "database": "twitterClone",
-  //   "host": "127.0.0.1",
-  //   "dialect": "postgres",
-  //   "logging": false
-  // },
+  "development": {
+    "username": "postgres",
+    "password": "090696",
+    "database": "twitterClone",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "logging": false
+  },
   "production": {
     "username": process.env.RDS_USERNAME,
     "password": process.env.RDS_PASSWORD,
