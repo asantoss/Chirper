3b063e54d2d7ceba0e9d66c2bf9226bd95b93e46
Alexander Santos <alexsantosantana@live.com>
2019-08-18 22:08:57 -0400

    make it better

--- config/config.js
+++ config/config.js
@@ -8,13 +8,13 @@ module.exports = {
     "logging": false
   },
   "production": {
-    "username": process.env.RDS_USERNAME,
-    "password": process.env.RDS_PASSWORD,
-    "database": process.env.RDS_DB_NAME,
-    "host": process.env.RDS_HOSTNAME,
-    "dialect": "postgres"
-    //! For HEROKU
-    // "use_env_variable": "DATABASE_URL",
+    // "username": process.env.RDS_USERNAME,
+    // "password": process.env.RDS_PASSWORD,
+    // "database": process.env.RDS_DB_NAME,
+    // "host": process.env.RDS_HOSTNAME,
     // "dialect": "postgres"
+    //! For HEROKU
+    "use_env_variable": "DATABASE_URL",
+    "dialect": "postgres"
   }
 }
