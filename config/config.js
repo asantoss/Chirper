9f4a09119c2b6418c2e6009e857cb936a864523e
Alexander Santos <alexsantosantana@live.com>
2019-08-21 10:13:26 -0400

    AWS so hard

--- config/config.js
+++ config/config.js
@@ -10,7 +10,7 @@ module.exports = {
   "production": {
     "username": process.env.RDS_USERNAME,
     "password": process.env.RDS_PASSWORD,
-    "database": "aa19m8ws8ueib14",
+    "database": process.env.RDS_DB_NAME,
     "host": process.env.RDS_HOSTNAME,
     "dialect": "postgres"
     // ! For HEROKU
