41189ce1975c0ef27036eabd9daff2dd07530d9d
Alexander Santos <alexsantosantana@live.com>
2019-08-21 10:47:08 -0400

    AWS Deployment

--- config/config.js
+++ config/config.js
@@ -1,11 +1,10 @@
 module.exports = {
   "development": {
-    "username": "postgres",
-    "password": "090696",
-    "database": "twitterClone",
-    "host": "127.0.0.1",
-    "dialect": "postgres",
-    "logging": false
+    "username": process.env.RDS_USERNAME,
+    "password": process.env.RDS_PASSWORD,
+    "database": process.env.RDS_DB_NAME,
+    "host": process.env.RDS_HOSTNAME,
+    "dialect": "postgres"
   },
   "production": {
     "username": process.env.RDS_USERNAME,
